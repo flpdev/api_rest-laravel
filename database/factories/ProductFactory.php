1b979f784da6ff76519b2326eb74daa25a5e00ae
Felipe Moreira <fm.felipe@gmail.com>
2020-07-11 22:06:14 -0400

    configuração seed, factory

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -5,8 +5,10 @@
 use App\Model;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(\App\Product::class, function (Faker $faker) {
     return [
-        //
+        'name' => $faker->name,
+        'price' => $faker->randomFloat(2,0,8),
+        'description' => $faker->text
     ];
 });
