7b671efa1be0348d76c56afac0a4adbfad6d3e22
Felipe Moreira <fm.felipe@gmail.com>
2020-07-22 14:34:27 -0400

    Criada classe return error, alterações controller product e rotas

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\API\ApiError;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -12,17 +14,41 @@ class ProductController extends Controller
     private $product;
 
     public function __construct(Product $product){
+
         $this->product = $product;
+
     }
 
     public function index(){
-        return $this->product->all();
+
+        //$data = ['data' => $this->product->paginate(5)];
+        // PAGINATE, FUNCIONALIDADE DO LARAVEL QUE PERMITE A PAGINAÇÃO DOS RESULTADOS
+                
+        return response()->json($this->product->paginate(5));
+
     }
 
     public function show(Product $id){
-        return $id;
+
+        $data = ['data' => $id];
+
+        return response()->json($data);
     }
 
-    
+    public function store(Request $request){
+
+        try {
+            $ProductData = $request->all();
+            $this->product->create($ProductData);
+        } catch (\Exception $e) {
+            if(config('app.debug')){
+                return response()->json(ApiError::errorMessage($e->getMessage(),1010));    
+            }
+            return response()->json(ApiError::errorMessage('Erro ao realizar a operação', 1010));
+        }
+
+    }
+
+
 
 }
